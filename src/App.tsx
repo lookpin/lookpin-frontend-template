412e001caedce5bd674167255cb8d62cef7f40cd
chkim116 <dudnqnfqlc@naver.com>
2022-08-16 19:10:30 +0900

    refactor: 잘못 푸쉬한 라인 제거

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import { renderRouteSystem } from 'ts-fe-toolkit';
 
-import { Redirect, Route } from 'react-router-dom';
 import { featureRoutes } from '@features/feat-routes';
 
 export function App() {
@@ -9,7 +8,6 @@ export function App() {
       {renderRouteSystem(featureRoutes, true, () => (
         <div>error</div>
       ))}
-      <Route path="/main" component={() => <Redirect to="/other" />} />
     </>
   );
 }
