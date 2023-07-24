e46e5001482d3fe8705262ef0a62cec6f778e5f6
theson <junhyeon.son@lookpin.co.kr>
2023-07-25 07:58:50 +0900

    feat(core): 샘플용 더미 레포지토리 추가 외
    - 폴더명칭 오타 수정

--- src/core/repo.ts
+++ src/core/repo.ts
@@ -1,5 +1,7 @@
-import { rootRepository } from './repostiory/root.repo';
+import { dummyRepository } from './repositories/dummy.repo';
+import { rootRepository } from './repositories/root.repo';
 
 export default {
   root: rootRepository,
+  dummy: dummyRepository,
 };
